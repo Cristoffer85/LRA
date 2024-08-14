8d97fa5ae62e5a58dffa12ddbcbfbb4acb9c71b1
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-08-14 14:01:37 +0200

    Initial commit

---
+++ .venv/main.py
@@ -0,0 +1,29 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+from screens import StartScreen, OptionsScreen
+
+# Load the kv files for each screen
+Builder.load_file('KV/startscreen.kv')
+Builder.load_file('KV/optionsscreen.kv')
+Builder.load_file('KV/widgets.kv')
+
+class MyScreenManager(ScreenManager):
+    pass
+
+class MyApp(App):
+    def build(self):
+        self.sm = MyScreenManager()
+        self.sm.add_widget(StartScreen(name='start'))
+        self.sm.add_widget(OptionsScreen(name='FUNRace'))
+        return self.sm
+
+    def option_selected(self, instance):
+        print(f'{instance.text} selected')
+
+    def go_back(self):
+        if self.sm.current != 'start':
+            self.sm.current = 'start'
+
+if __name__ == '__main__':
+    MyApp().run()
---
+++ .venv/screens.py
@@ -0,0 +1,7 @@
+from kivy.uix.screenmanager import Screen
+
+class StartScreen(Screen):
+    pass
+
+class OptionsScreen(Screen):
+    pass
